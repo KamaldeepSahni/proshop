7a249d3e917d428096293b27d498367ae36c2057
Kamaldeep <ksahni687@gmail.com>
2020-12-22 00:31:38 +0530

    Loader & Error Components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import { listProducts } from '../actions/productActions';
 
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+
 import Product from '../components/Product';
 
 const HomeScreen = () => {
@@ -20,9 +23,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loaing...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map(product => (
