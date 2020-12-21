ef42533aa0bef551217fc6eecbd09d5567b47f7d
Kamaldeep <ksahni687@gmail.com>
2020-12-22 01:10:42 +0530

    Add to Cart Func

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Footer from './components/Footer';
 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Container>
           <Route exact path="/" component={HomeScreen} />
           <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
